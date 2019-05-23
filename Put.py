d6420abcdcb6e7a923d0bd4227167d7daf9ce6f8
Felipe Oliveira Bueno de Camargo <felipe.c@outlook.com>
2019-05-23 15:34:38 -0300

    não foi  ainda

--- Put.py
+++ Put.py
@@ -1,22 +1,24 @@
-# importing the requests library 
-import requests 
-  
-# defining the api-endpoint  
-API_ENDPOINT = "https://api.codenation.dev/v1/challenge/dev-ps/generate-data?token=099bb428faac4b5fa39b8514f789502a1a779258"
-  
-  
-# data to be sent to api 
+import requests
+
+info = {
+    'var1' : 'this',
+    'var2'  : 'that',
+}
+
 data = {
     "numero_casas": 3,
     "token": "099bb428faac4b5fa39b8514f789502a1a779258",
     "cifrado": "wkh ehvw zdb wr pdnh brxu guhdpv frph wuxh lv wr zdnh xs. pxulho vlhehuw",
-    "decifrado": "the best way to make your dreams come true is to wake up. muriel siebert",
-    "resumo_criptografico": "4b649e40a0a1f2e6776c1291a0c81452dd697fb7"
+    "decifrado": "",
+    "resumo_criptografico": ""
  }
-  
-# sending post request and saving response as response object 
-r = requests.post(url = API_ENDPOINT, data = data) 
-  
-# extracting response text  
-pastebin_url = r.text 
-print(pastebin_url) 
+
+url = 'https://api.codenation.dev/v1/challenge/dev-ps/submit-solution?token=099bb428faac4b5fa39b8514f789502a1a779258'
+
+headers = {'Content-type': 'multipart/form-data'}
+
+answer = {'answer': open('answer.json', 'rb')}
+
+r = requests.post(url, files=answer, data=data, headers=headers)
+
+print(r.content)
